a0dd867fcb4b23b328aed03c257b745a205de7bc
aanyagupta3934 <aanyalee2902@gmail.com>
2026-04-09 10:15:27 +0530

    Add files via upload

---
+++ assignment_9.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int num, originalNum, remainder, result = 0;
+
+    printf("Enter a three-digit integer: ");
+    scanf("%d", &num);
+
+    originalNum = num;
+
+    while (originalNum != 0) {
+        // remainder contains the last digit
+        remainder = originalNum % 10;
+        
+        // adding the cube of the last digit to result
+        result += remainder * remainder * remainder;
+        
+        // removing last digit from the orignal number
+        originalNum /= 10;
+    }
+
+    if (result == num)
+        printf("%d is an Armstrong number.\n", num);
+    else
+        printf("%d is not an Armstrong number.\n", num);
+
+    return 0;
+}
